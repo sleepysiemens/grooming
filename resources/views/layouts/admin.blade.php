110ff9ce954209108ae07c153a8833b64a5e51c2
sleepysiemens <2001sema@gmail.com>
2024-04-13 21:29:45 +0700

    second commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -38,7 +38,6 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{route('admin.index')}}" class="brand-link">
-            <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">{{__('Админ панель')}}</span>
         </a>
         @if(auth()->user()->is_admin)
